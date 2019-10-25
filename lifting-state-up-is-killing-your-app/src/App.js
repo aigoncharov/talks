44997b3a92501ecf8e0b15ddb22a762ad26699a1
Andrey Goncharov <andrey@goncharov.page>
2019-10-25 10:49:09 +0300

    Add "Lifting state up is killing your app" #6
    Add Field

--- lifting-state-up-is-killing-your-app/src/App.js
+++ lifting-state-up-is-killing-your-app/src/App.js
@@ -1,49 +1,57 @@
-import React, { useState, useCallback, memo } from 'react'
+import React from 'react'
 
 import './App.css'
 
 const size = 10
 
-const Cell = memo(({ content, setContent, cellI, rowI }) => {
+class Field {
+  constructor(fieldSize) {
+    this.size = fieldSize
+    // Copy-paste from `initialState`
+    this.data = new Array(this.size).fill(new Array(this.size).fill(undefined))
+  }
+
+  cellContent(rowI, cellI) {
+    return this.data[rowI][cellI]
+  }
+
+  // Copy-paste from  old `setCell`
+  setCell(rowI, cellI, newContent) {
+    console.log('setCell')
+    this.data = [
+      ...this.data.slice(0, rowI),
+      [
+        ...this.data[rowI].slice(0, cellI),
+        newContent,
+        ...this.data[rowI].slice(cellI + 1),
+      ],
+      ...this.data.slice(rowI + 1),
+    ]
+  }
+
+  map(cb) {
+    return this.data.map(cb)
+  }
+}
+
+const field = new Field(size)
+
+const Cell = ({ cellI, rowI }) => {
   console.log('cell rendered')
   return (
-    <div className="cell" onClick={() => setContent(rowI, cellI, '✔')}>
-      {content}
+    <div className="cell" onClick={() => field.setCell(rowI, cellI, '✔')}>
+      {field.cellContent(rowI, cellI)}
     </div>
   )
-})
-
-const initialField = new Array(size).fill(new Array(size).fill(undefined))
+}
 
 export const App = () => {
-  const [field, setField] = useState(initialField)
-
-  const setCell = useCallback(
-    (rowI, cellI, newContent) =>
-      setField((oldField) => [
-        ...oldField.slice(0, rowI),
-        [
-          ...oldField[rowI].slice(0, cellI),
-          newContent,
-          ...oldField[rowI].slice(cellI + 1),
-        ],
-        ...oldField.slice(rowI + 1),
-      ]),
-    [],
-  )
-
   return (
     <div>
       {field.map((row, rowI) => (
         <div className="row" key={rowI}>
           {row.map((cell, cellI) => (
-            <Cell
-              key={`row${rowI}cell${cellI}`}
-              content={cell}
-              rowI={rowI}
-              cellI={cellI}
-              setContent={setCell}
-            />
+            <Cell key={`row${rowI}cell${cellI}`} rowI={rowI} cellI={cellI} />
           ))}
         </div>
       ))}
